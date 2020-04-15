9f3ec212c549d58dff81367767ac71499577e77d
Michael <fingusmicroblog@gmail.com>
2020-04-15 23:52:08 +0500

    я восстановил после краша

--- config.py
+++ config.py
@@ -20,3 +20,4 @@ class Config(object):
     LANGUAGES = ['en', 'es']
     YA_TRANSLATOR_KEY = os.environ.get('YA_TRANSLATOR_KEY')
     ELASTICSEARCH_URL = os.environ.get('ELASTICSEARCH_URL')
+    LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
